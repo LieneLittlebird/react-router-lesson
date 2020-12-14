3d308db60dfd3186b14cc1a399cecadf09e552b4
Liene Putniņa <66679828+LieneLittlebird@users.noreply.github.com>
2020-12-14 20:31:39 +0200

    initial commit with routers

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <Router>
+                <Header />
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/">
+                            This is the index page
+                        </Route>
+                        <Route exact path="/products">
+                            This is the products page
+                        </Route>
+                        <Route exact path="/about-us">
+                            This is the about-us page
+                        </Route>
+                    </Switch>
+                </div>
+                <Footer />
+            </Router>
+        </div>
+    );
 }
 
 export default App;
